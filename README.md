5f8089603905005cf8c29dc21da25ac754f9671f
Petersen <itpetersen@uiowa.edu>
2023-06-07 14:50:42 -0500

    20230607 - initial code

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # spatial-recall
-jsPsych code for spatial recall task
+jsPsych code for spatial recall task; adapted from code by Sam Zorowitz:
+
+- https://nivlab.github.io/jspsych-demos/
+- https://github.com/nivlab/jspsych-demos/tree/main/tasks/spatial-recall
---
+++ js/init-jspsych.js
@@ -0,0 +1,14 @@
+// Initialize jsPsych.
+var jsPsych = initJsPsych({
+  show_progress_bar: true,
+  on_finish: function() {
+
+    // Add interactions to the data variable
+    var interaction_data = jsPsych.data.getInteractionData();
+    jsPsych.data.get().addToLast({interactions: interaction_data.json()});
+
+    // Display jsPsych data in viewport.
+    jsPsych.data.displayData();
+
+  }
+});
